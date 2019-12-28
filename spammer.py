f855749095bbc7b10a1722453d5edb8a3125cf3a
robert169 <48752420+robert169@users.noreply.github.com>
2019-12-28 21:40:55 +0200

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,3 +1,4 @@
+import random
 import discord
 import asyncio
 import sys
@@ -7,54 +8,111 @@ import os
 import json
 import threading
 import time
-from os import system
 from subprocess import call 
 import ctypes
-from time import sleep 
+from time import sleep
 from colorama import Fore,init
 import sys
 import threading
-version= "1.0"
+ui = "Hans spammer started:"
+version= "1.1"
+clear =  lambda: os.system("cls")
+blue = Fore.BLUE
+white = Fore.WHITE
+red = Fore.WHITE
+pink = Fore.MAGENTA
+green = Fore.GREEN
+black = Fore.BLACK
+    
 
-alb = Fore.WHITE
-rosu = Fore.RED
-verde = Fore.GREEN
-init()
-clear = lambda: os.system("cls")
+def leave_function():
+    link = input(Fore.CYAN+f'[!] Discord Server Id: ')
+    if len(link) > 0:
+        apilink = f"https://discordapp.com/api/v7/users/@me/guilds/{link}"
 
-version = "1.0"
-
-ui = f'''{Fore.CYAN}
-
-                     _   _                                                                 
-                    | | | |                                                                
-                    | |_| | __ _ _ __  ___   ___ _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
-                    |  _  |/ _` | '_ \/ __| / __| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
-                    | | | | (_| | | | \__ \ \__ \ |_) | (_| | | | | | | | | | | |  __/ |   
-                    \_| |_/\__,_|_| |_|___/ |___/ .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
-                                                | |                                        
-                                                |_|                         Version: {version}               
-
-                                                                                                                        
-    '''
-
-
+        print(f"{Fore.CYAN}Bots leaving from:{Fore.WHITE} {link}")
+        s = 0
+        with open('in/tokens.txt','r') as handle:
+            tokens = handle.readlines()
+            for x in tokens:
+                token = x.rstrip()
+                headers={
+                        "Authorization": token
+                    }
+                a = requests.delete(apilink, headers=headers)
+                if "204" in a:
+                    s+= 1
+                else:
+                    pass
+            print (f"{Fore.WHITE}[{Fore.GREEN}{s}{Fore.WHITE}/{Fore.GREEN}{len(tokens)}{Fore.WHITE}] left from {link}!")
+            time.sleep(1)
+            input()
+            sys.exit()
+            exit()
+    else:
+        sys.exit()
+        exit()
+def token_checker():
+    open("out/valid.txt", "a")
+    open("out/invalid.txt", "a")
+    check_link = "https://discordapp.com/api/v6/invite/pXdxXCC"
+    s = 0
+    b = 0
+    with open('in/tokens.txt','r') as handle:
+        tokens = handle.readlines()
+        for x in tokens:
+            token = x.strip()
+            headers={
+                'Authorization': token
+            }
+            a = requests.post(check_link, headers=headers)
+            #with open("logs.txt", "a") as n:
+            #    n.write(f"{a.json()}\n")
+            if "You need to verify your account in order to perform this action." in a.text:
+                print(f"{white}[{red}DEAD{white}] {red}{x}{white}".replace("\n", ""))
+                with open("out/invalid.txt", "a") as m:
+                    m.write(f"{x}")
+                b += 1
+            elif "Unauthorized" in a.text:
+                print(f"{white}[{red}DEAD{white}] {red}{x}{white}".replace("\n", ""))
+                with open("out/invalid.txt", "a") as m:
+                    m.write(f"{x}")
+                b += 1
+            elif "Access denied" in a.text:
+                print("You got banned kek")
+                input()
+                sys.exit()
+                exit()
+            else:
+                s += 1
+                print(f"{white}[{green}ALIVE{white}] {green}{x}{white}".replace("\n", ""))
+                with open("out/valid.txt", "a") as m:
+                    m.write(f"{x}")
+            ctypes.windll.kernel32.SetConsoleTitleW(f"Token Checker | Valid: {s} Invalid: {b}")
+        print (f"\n[!] Valid: {green}{s}{white} Invalid: {red}{b} {Fore.CYAN}Total: {len(tokens)}")
+        input(">> Press Enter to exit")
+        sys.exit()
+        exit()            
 
 def spammer():
     clear()
-    ctypes.windll.kernel32.SetConsoleTitleW("Hans spammer | v{}".format(version))
+    ctypes.windll.kernel32.SetConsoleTitleW("Hans Spammer | v{}".format(version))
     init()
     print(ui)
     print()
 
+    print(Fore.BLUE+f'[0]{Fore.YELLOW} Token checker (proxyless)')
     print(Fore.BLUE+f'[1]{Fore.WHITE} Join server')
     print(Fore.BLUE+f'[2]{Fore.WHITE} Spam server')
-    print(Fore.BLUE+f'[3]{Fore.WHITE} Spam friend request')
-    print()
+    print(Fore.BLUE+f'[3]{Fore.WHITE} Spam friend reuqest')
+    #print(Fore.BLUE+f'[4]{Fore.WHITE} Dm spam')
+    print(Fore.BLUE+f'[4]{Fore.WHITE} Server leave')
+
+        
 
 
     init()
-    print(Fore.CYAN + f'Choose an option:{Fore.WHITE} ', end='' + Fore.WHITE)
+    print(Fore.CYAN + f'[!] Choose an option:{Fore.WHITE} ', end='')
     option = str(input())
 
 
@@ -65,22 +123,25 @@ def spammer():
 
     token1 = []
     def load_token1():
-                with open('tokens.txt', 'r') as f:
+                with open('in/tokens.txt', 'r') as f:
                         for x in f.readlines():
                                 token1.append(x.replace('\n',''))
 
 
 
-
-    if option =='1':
-        link = input(Fore.CYAN+f'Discord Invite Link(link):{Fore.WHITE} ')
+    if option == "0":
+        clear()
+        threading.Thread(target=token_checker, args=()).start()
+        return
+    elif option =='1':
+        link = input(Fore.CYAN+f'[!] Discord Invite Link(link):{Fore.WHITE} ')
         if len(link) > 6:
             link = link[19:]
             apilink = "https://discordapp.com/api/v6/invite/" + str(link)
 
             print(f"{Fore.CYAN}Adding bots to server:{Fore.WHITE} {link}")
             s = 0
-            with open('tokens.txt','r') as handle:
+            with open('in/tokens.txt','r') as handle:
                     tokens = handle.readlines()
                     for x in tokens:
                         token = x.rstrip()
@@ -113,7 +174,7 @@ def spammer():
         m = 0
         gay1 = input(Fore.BLUE+f'Discord name:{Fore.WHITE} ')
         message2 = input(Fore.BLUE+f'Discrim:{Fore.WHITE} ')
-        with open('tokens.txt', 'r') as handle:
+        with open('in/tokens.txt', 'r') as handle:
             token1 = handle.readlines()
         for x in token1:
             url = 'https://discordapp.com/api/v6/users/@me/relationships'
@@ -129,7 +190,11 @@ def spammer():
         input(Fore.WHITE+"Press enter to exit")
         sys.exit()
         exit()
-       
+    
+    elif option == "4":
+        clear()
+        threading.Thread(target=leave_function, args=()).start()
+        return
     else:
         sys.exit()
         exit()
@@ -166,6 +231,4 @@ def spammer():
                         time.sleep(0.25)
 
     sender()
-
-   
 spammer()
